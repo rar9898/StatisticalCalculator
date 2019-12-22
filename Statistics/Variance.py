ef9ec14088bc48b6f4c56ca33c54260ad90a4dfa
ruchi rawat <risharawat2422@gmail.com>
2019-12-22 12:33:54 -0500

    Task: updated code for variance.py

---
+++ Statistics/Variance.py
@@ -0,0 +1,7 @@
+from Calculator.Square import square
+
+from Statistics.StandardDeviation import standard_deviation
+
+
+def variance(numbers):  # complete
+    return square(standard_deviation(numbers))
